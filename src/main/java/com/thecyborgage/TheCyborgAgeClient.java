72bad560443eeea36b865d89ab0691198e41a480
omerbenda <omerbenda@gmail.com>
2025-09-22 12:49:33 +0300

    feat(TheCyborgAgeMod.java): Add base neoforge mod files

---
+++ src/main/java/com/thecyborgage/TheCyborgAgeClient.java
@@ -0,0 +1,17 @@
+package com.thecyborgage;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod(value = TheCyborgAgeMod.MOD_ID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = TheCyborgAgeMod.MOD_ID, value = Dist.CLIENT)
+public class TheCyborgAgeClient {
+  public TheCyborgAgeClient(ModContainer container) {}
+
+  @SubscribeEvent
+  static void onClientSetup(FMLClientSetupEvent event) {}
+}
---
+++ src/main/java/com/thecyborgage/TheCyborgAgeMod.java
@@ -0,0 +1,17 @@
+package com.thecyborgage;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+
+@Mod(TheCyborgAgeMod.MOD_ID)
+public class TheCyborgAgeMod {
+  public static final String MOD_ID = "thecyborgage";
+  public static final Logger LOGGER = LogUtils.getLogger();
+
+  public TheCyborgAgeMod(IEventBus modEventBus, ModContainer modContainer) {}
+}
