37970d043725b2bd86913e3bf08dd2437ce544c3
omerbenda <omerbenda@gmail.com>
2025-10-08 20:07:04 +0300

    feat(cyborg entity resources): Add texture for cyborg entity

--- src/main/java/com/thecyborgage/client/renderers/CyborgRenderer.java
+++ src/main/java/com/thecyborgage/client/renderers/CyborgRenderer.java
@@ -1,5 +1,6 @@
 package com.thecyborgage.client.renderers;
 
+import com.thecyborgage.TheCyborgAgeMod;
 import com.thecyborgage.entities.CyborgEntity;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.ModelLayers;
@@ -8,13 +9,15 @@ import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
 public class CyborgRenderer extends MobRenderer<CyborgEntity, PlayerModel<CyborgEntity>> {
+  private static final ResourceLocation TEXTURE =
+      ResourceLocation.fromNamespaceAndPath(TheCyborgAgeMod.MOD_ID, "textures/entity/cyborg.png");
+
   public CyborgRenderer(EntityRendererProvider.Context context) {
     super(context, new PlayerModel<>(context.bakeLayer(ModelLayers.PLAYER), false), 0.5F);
   }
 
   @Override
   public ResourceLocation getTextureLocation(CyborgEntity cyborgEntity) {
-    return ResourceLocation.fromNamespaceAndPath(
-        "minecraft", "textures/entity/player/wide/steve.png");
+    return TEXTURE;
   }
 }
