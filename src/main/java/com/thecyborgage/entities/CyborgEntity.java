3bf194d52144176ef65c57f0d20f0567dcb441fc
omerbenda <omerbenda@gmail.com>
2025-10-13 15:13:38 +0300

    feat(CyborgEntity.java): Add random stroll to cyborg entity

--- src/main/java/com/thecyborgage/entities/CyborgEntity.java
+++ src/main/java/com/thecyborgage/entities/CyborgEntity.java
@@ -4,6 +4,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
@@ -21,6 +22,7 @@ public class CyborgEntity extends Monster {
     this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0, true));
     this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 8.0F));
     this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
+    this.goalSelector.addGoal(8, new RandomStrollGoal(this, 0.6));
     this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
   }
 }
