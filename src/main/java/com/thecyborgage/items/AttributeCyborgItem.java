2f59f03f3f6cae9e799ead6af49aa7afec402a0d
omerbenda <omerbenda@gmail.com>
2025-11-21 16:45:08 +0200

    refactor(AttributeCyborgItem.java): Add class for attribute cyborg items

---
+++ src/main/java/com/thecyborgage/items/AttributeCyborgItem.java
@@ -0,0 +1,81 @@
+package com.thecyborgage.items;
+
+import com.thecyborgage.TCACuriosHelper;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.*;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
+
+public class AttributeCyborgItem extends Item implements ICurioItem {
+  private final ResourceLocation modifierResLoc;
+  private final Holder<Attribute> attribute;
+  private final double amount;
+  private final AttributeModifier.Operation operation;
+  private final int energyUsage;
+
+  public AttributeCyborgItem(
+      Properties properties,
+      ResourceLocation modifierResLoc,
+      Holder<Attribute> attribute,
+      double amount,
+      AttributeModifier.Operation operation,
+      int energyUsage) {
+    super(properties);
+
+    this.modifierResLoc = modifierResLoc;
+    this.attribute = attribute;
+    this.amount = amount;
+    this.operation = operation;
+    this.energyUsage = energyUsage;
+  }
+
+  public boolean shouldConsumeEnergy(SlotContext slotContext, ItemStack stack) {
+    return true;
+  }
+
+  @Override
+  public void curioTick(SlotContext slotContext, ItemStack stack) {
+    ICurioItem.super.curioTick(slotContext, stack);
+
+    LivingEntity entity = slotContext.entity();
+    AttributeMap attributes = entity.getAttributes();
+    AttributeInstance attributeInstance = attributes.getInstance(this.attribute);
+
+    if (attributeInstance == null) {
+      return;
+    }
+
+    if (TCACuriosHelper.consumeEntityCoreEnergy(entity, this.energyUsage, true)) {
+      attributeInstance.addOrUpdateTransientModifier(this.createAttributeModifier());
+
+      if (this.shouldConsumeEnergy(slotContext, stack)) {
+        TCACuriosHelper.consumeEntityCoreEnergy(entity, this.energyUsage);
+      }
+    } else {
+      attributeInstance.removeModifier(this.modifierResLoc);
+    }
+  }
+
+  @Override
+  public void onUnequip(SlotContext slotContext, ItemStack newStack, ItemStack stack) {
+    ICurioItem.super.onUnequip(slotContext, newStack, stack);
+
+    LivingEntity entity = slotContext.entity();
+    AttributeMap attributes = entity.getAttributes();
+    AttributeInstance attributeInstance = attributes.getInstance(this.attribute);
+
+    if (attributeInstance == null) {
+      return;
+    }
+
+    attributeInstance.removeModifier(this.modifierResLoc);
+  }
+
+  private AttributeModifier createAttributeModifier() {
+    return new AttributeModifier(this.modifierResLoc, this.amount, this.operation);
+  }
+}
