f272ef6e67001758ea698f7c488084251465dc22
omerbenda <omerbenda@gmail.com>
2025-11-27 18:16:09 +0200

    feat(TCAEntityEvents.java): Add consuming energy on jump

--- src/main/java/com/thecyborgage/events/TCAEntityEvents.java
+++ src/main/java/com/thecyborgage/events/TCAEntityEvents.java
@@ -2,6 +2,7 @@ package com.thecyborgage.events;
 
 import com.thecyborgage.TCACuriosHelper;
 import com.thecyborgage.TheCyborgAgeMod;
+import com.thecyborgage.config.TCAServerConfig;
 import com.thecyborgage.entities.CyborgEntity;
 import com.thecyborgage.init.TCAEntities;
 import com.thecyborgage.init.TCAItems;
@@ -17,6 +18,7 @@ import net.neoforged.neoforge.energy.IEnergyStorage;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
 import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
 import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
+import net.neoforged.neoforge.event.entity.living.LivingEvent;
 
 import java.util.Optional;
 
@@ -51,7 +53,29 @@ public class TCAEntityEvents {
         IEnergyStorage coreEnergyStorage = optionalCoreEnergyStorage.get();
 
         TCACuriosHelper.consumeEntityCoreEnergy(
-            entity, Math.min(1000, coreEnergyStorage.getEnergyStored()));
+            entity,
+            Math.min(
+                TCAServerConfig.CONFIG.energyArmorHitDischarge.getAsInt(),
+                coreEnergyStorage.getEnergyStored()));
+      }
+    }
+  }
+
+  @SubscribeEvent
+  public static void onEntityJump(LivingEvent.LivingJumpEvent evt) {
+    LivingEntity entity = evt.getEntity();
+    Optional<IEnergyStorage> optionalCoreEnergyStorage =
+        TCACuriosHelper.getEntityCoreEnergyStorage(entity);
+
+    if (optionalCoreEnergyStorage.isPresent()) {
+      Optional<ItemStack> optionalJumpLeg =
+          TCACuriosHelper.getEntityCurioItem(entity, TCAItems.CYBORG_JUMP_LEG.get());
+
+      if (optionalJumpLeg.isPresent()) {
+        IEnergyStorage coreEnergyStorage = optionalCoreEnergyStorage.get();
+
+        TCACuriosHelper.consumeEntityCoreEnergy(
+            entity, Math.min(350, coreEnergyStorage.getEnergyStored()));
       }
     }
   }
--- src/main/java/com/thecyborgage/items/CyborgJumpLegItem.java
+++ src/main/java/com/thecyborgage/items/CyborgJumpLegItem.java
@@ -19,7 +19,7 @@ public class CyborgJumpLegItem extends AttributeCyborgItem {
 
   @Override
   public double getAmount(SlotContext slotContext, ItemStack stack) {
-    return 1;
+    return 0.6;
   }
 
   @Override
