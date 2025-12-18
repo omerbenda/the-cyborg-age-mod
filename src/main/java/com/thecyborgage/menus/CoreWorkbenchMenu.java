7d2bede31365a539a3753bd09c8fe93bb0569206
omerbenda <omerbenda@gmail.com>
2025-12-18 16:59:35 +0200

    fix(CoreWorkbenchMenu.java): Fix returning items to player when closing the core workbench menu

--- src/main/java/com/thecyborgage/menus/CoreWorkbenchMenu.java
+++ src/main/java/com/thecyborgage/menus/CoreWorkbenchMenu.java
@@ -4,7 +4,6 @@ import com.thecyborgage.init.TCABlocks;
 import com.thecyborgage.init.TCADataComponents;
 import com.thecyborgage.init.TCAItems;
 import com.thecyborgage.init.TCAMenuTypes;
-import com.thecyborgage.items.CyborgCoreItem;
 import com.thecyborgage.menus.slots.CoreBatterySlot;
 import com.thecyborgage.menus.slots.FilterSlot;
 import net.minecraft.world.Container;
@@ -16,13 +15,12 @@ import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.ItemContainerContents;
-import net.neoforged.neoforge.items.ItemHandlerHelper;
 
 public class CoreWorkbenchMenu extends AbstractContainerMenu {
   private final Inventory inventory;
   private final ContainerLevelAccess access;
+  private final SimpleContainer coreContainer;
   private final SimpleContainer coreInventory;
-  private boolean hasSetContents;
 
   public CoreWorkbenchMenu(int containerId, Inventory inventory) {
     this(containerId, inventory, ContainerLevelAccess.NULL);
@@ -31,10 +29,11 @@ public class CoreWorkbenchMenu extends AbstractContainerMenu {
   public CoreWorkbenchMenu(int containerId, Inventory inventory, ContainerLevelAccess access) {
     super(TCAMenuTypes.CORE_WORKBENCH_MENU.get(), containerId);
 
-    this.hasSetContents = false;
     this.inventory = inventory;
     this.access = access;
-    this.coreInventory = new SimpleContainer(5);
+    this.coreContainer = new SimpleContainer(1);
+    this.coreContainer.addListener(this::onCoreContainerChanged);
+    this.coreInventory = new SimpleContainer(4);
     this.coreInventory.addListener(this::onCoreInventoryChanged);
 
     this.addWorkbenchSlots();
@@ -42,13 +41,13 @@ public class CoreWorkbenchMenu extends AbstractContainerMenu {
   }
 
   private void addWorkbenchSlots() {
-    this.addSlot(new FilterSlot(TCAItems.CYBORG_CORE.get(), this.coreInventory, 0, 80, 35));
+    this.addSlot(new FilterSlot(TCAItems.CYBORG_CORE.get(), this.coreContainer, 0, 80, 35));
 
-    this.addSlot(new CoreBatterySlot(this, this.coreInventory, 1, 41, 21));
-    this.addSlot(new CoreBatterySlot(this, this.coreInventory, 2, 41, 49));
+    this.addSlot(new CoreBatterySlot(this, this.coreInventory, 0, 41, 21));
+    this.addSlot(new CoreBatterySlot(this, this.coreInventory, 1, 41, 49));
 
-    this.addSlot(new CoreBatterySlot(this, this.coreInventory, 3, 119, 21));
-    this.addSlot(new CoreBatterySlot(this, this.coreInventory, 4, 119, 49));
+    this.addSlot(new CoreBatterySlot(this, this.coreInventory, 2, 119, 21));
+    this.addSlot(new CoreBatterySlot(this, this.coreInventory, 3, 119, 49));
   }
 
   private void addInventorySlots() {
@@ -63,15 +62,11 @@ public class CoreWorkbenchMenu extends AbstractContainerMenu {
     }
   }
 
-  private void onCoreInventoryChanged(Container container) {
-    ItemStack coreSlotStack = this.coreInventory.getItem(0);
+  private void onCoreContainerChanged(Container container) {
+    ItemStack coreSlotStack = this.coreContainer.getItem(0);
 
-    if (coreSlotStack.isEmpty() || !(coreSlotStack.getItem() instanceof CyborgCoreItem)) {
-      if (this.hasSetContents) {
-        this.hasSetContents = false;
-        this.coreInventory.clearContent();
-      }
-
+    if (coreSlotStack.isEmpty()) {
+      this.coreInventory.clearContent();
       return;
     }
 
@@ -81,24 +76,27 @@ public class CoreWorkbenchMenu extends AbstractContainerMenu {
       return;
     }
 
-    if (!this.hasSetContents) {
-      this.hasSetContents = true;
-      int slotIndex = 1;
+    int slotIndex = 0;
+    for (ItemStack itemStack : contents.nonEmptyItemsCopy()) {
+      this.coreInventory.setItem(slotIndex, itemStack);
+      slotIndex++;
+    }
+  }
 
-      for (ItemStack itemStack : contents.nonEmptyItemsCopy()) {
-        this.coreInventory.setItem(slotIndex, itemStack);
-        slotIndex++;
-      }
+  private void onCoreInventoryChanged(Container container) {
+    ItemStack coreSlotStack = this.coreContainer.getItem(0);
+
+    if (coreSlotStack.isEmpty()) {
+      return;
     }
 
     ItemContainerContents newContents =
-        ItemContainerContents.fromItems(
-            this.coreInventory.getItems().stream().skip(1).limit(4).toList());
+        ItemContainerContents.fromItems(this.coreInventory.getItems().stream().limit(4).toList());
     coreSlotStack.set(TCADataComponents.ENERGY_ITEM_STORAGE, newContents);
   }
 
   public boolean hasCore() {
-    return this.hasSetContents;
+    return !this.coreContainer.isEmpty();
   }
 
   @Override
@@ -113,15 +111,7 @@ public class CoreWorkbenchMenu extends AbstractContainerMenu {
 
   @Override
   public void removed(Player player) {
-    ItemStack coreStack = this.coreInventory.getItem(0);
-
-    if (coreStack.isEmpty()) {
-      return;
-    }
-
-    ItemHandlerHelper.giveItemToPlayer(player, coreStack);
-    this.coreInventory.setItem(0, ItemStack.EMPTY);
-
     super.removed(player);
+    this.access.execute((pLevel, pPos) -> this.clearContainer(player, this.coreContainer));
   }
 }
