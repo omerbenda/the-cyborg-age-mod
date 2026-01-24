f32e806ea40af95d6efdeb36ddcc71f34fbb1aef
omerbenda <omerbenda@gmail.com>
2026-01-24 20:30:02 +0200

    fix(Cyborg jump leg): Add system message for cyborg jump leg state change

--- src/main/java/com/thecyborgage/network/ServerPayloadHandler.java
+++ src/main/java/com/thecyborgage/network/ServerPayloadHandler.java
@@ -2,6 +2,8 @@ package com.thecyborgage.network;
 
 import com.thecyborgage.init.TCAAttachments;
 import com.thecyborgage.network.packets.ToggleValuePayload;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 
@@ -12,7 +14,15 @@ public class ServerPayloadHandler {
           if (payload.value() == ToggleValuePayload.ToggleValue.CYBORG_JUMP_LEG) {
             Player player = context.player();
 
-            player.setData(TCAAttachments.CYBORG_JUMP_LEG_TOGGLE_STATE, payload.toggled());
+            boolean toggled = payload.toggled();
+
+            player.setData(TCAAttachments.CYBORG_JUMP_LEG_TOGGLE_STATE, toggled);
+            player.sendSystemMessage(
+                Component.translatable(
+                        toggled
+                            ? "thecyborgage.system.cyborg_jump_leg_state_enabled"
+                            : "thecyborgage.system.cyborg_jump_leg_state_disabled")
+                    .withStyle(ChatFormatting.GRAY));
           }
         });
   }
