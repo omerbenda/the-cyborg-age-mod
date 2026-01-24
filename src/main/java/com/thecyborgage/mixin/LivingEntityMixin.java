a23f5e5ea4fb76fbcb2204862809ce6b8e8cf90d
omerbenda <omerbenda@gmail.com>
2026-01-24 21:46:59 +0200

    fix(Active Camouflage): Add mixin to remove range for mob detection

--- src/main/java/com/thecyborgage/mixin/LivingEntityMixin.java
+++ src/main/java/com/thecyborgage/mixin/LivingEntityMixin.java
@@ -1,27 +1,23 @@
 package com.thecyborgage.mixin;
 
 import com.thecyborgage.init.TCAAttachments;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.neoforge.attachment.IAttachmentHolder;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Entity.class)
-public abstract class ActiveCamouflageMixin {
-  @Inject(method = "isInvisible", at = @At("RETURN"), cancellable = true)
-  private void activeCamouflageInvisibility(CallbackInfoReturnable<Boolean> info) {
-    if (info.getReturnValueZ()){
-      return;
-    }
-
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+  @Inject(method = "getArmorCoverPercentage", at = @At("HEAD"), cancellable = true)
+  private void activeCamouflageArmorCoverPercentage(CallbackInfoReturnable<Float> info) {
     IAttachmentHolder attachmentHolder = (IAttachmentHolder) this;
 
     // Check hasData first to reduce calling default value supplier
     if (attachmentHolder.hasData(TCAAttachments.ACTIVE_CAMOUFLAGE_STATE)) {
       if (attachmentHolder.getData(TCAAttachments.ACTIVE_CAMOUFLAGE_STATE)) {
-        info.setReturnValue(true);
+        info.setReturnValue(0.0F);
       }
     }
   }
