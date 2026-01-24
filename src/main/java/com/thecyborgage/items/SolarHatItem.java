c79b2e49cd48f4f3684f5e4e22bd7db6f5e9641a
omerbenda <omerbenda@gmail.com>
2026-01-24 18:05:46 +0200

    feat(Solar Hat, Thermal Generator and Player Radar): Add tooltips

--- src/main/java/com/thecyborgage/items/SolarHatItem.java
+++ src/main/java/com/thecyborgage/items/SolarHatItem.java
@@ -2,13 +2,18 @@ package com.thecyborgage.items;
 
 import com.thecyborgage.config.TCAServerConfig;
 import com.thecyborgage.TCACuriosHelper;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
+
+import java.util.List;
 
 public class SolarHatItem extends Item implements ICurioItem {
   public SolarHatItem(Properties properties) {
@@ -22,11 +27,24 @@ public class SolarHatItem extends Item implements ICurioItem {
     LivingEntity entity = slotContext.entity();
 
     if (receivesSun(entity.level(), entity.blockPosition())) {
-      TCACuriosHelper.addEntityCoreEnergy(entity, TCAServerConfig.CONFIG.solarHatChargeRate.getAsInt());
+      TCACuriosHelper.addEntityCoreEnergy(
+          entity, TCAServerConfig.CONFIG.solarHatChargeRate.getAsInt());
     }
   }
 
   private static boolean receivesSun(Level level, BlockPos blockPos) {
     return level.isDay() && level.canSeeSky(blockPos);
+  }
+
+  @Override
+  public void appendHoverText(
+      ItemStack stack,
+      TooltipContext context,
+      List<Component> tooltipComponents,
+      TooltipFlag tooltipFlag) {
+    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+
+    tooltipComponents.add(
+        Component.translatable("thecyborgage.solar_hat.tooltip").withStyle(ChatFormatting.GRAY));
   }
 }
--- src/main/java/com/thecyborgage/items/ThermalGeneratorItem.java
+++ src/main/java/com/thecyborgage/items/ThermalGeneratorItem.java
@@ -2,13 +2,18 @@ package com.thecyborgage.items;
 
 import com.thecyborgage.config.TCAServerConfig;
 import com.thecyborgage.TCACuriosHelper;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
+
+import java.util.List;
 
 public class ThermalGeneratorItem extends Item implements ICurioItem {
   public ThermalGeneratorItem(Properties properties) {
@@ -25,12 +30,26 @@ public class ThermalGeneratorItem extends Item implements ICurioItem {
     float temperature = level.getBiome(blockPos).value().getBaseTemperature();
     boolean isRaining = level.isRainingAt(blockPos);
 
-    double heatValue = temperature * TCAServerConfig.CONFIG.thermalGeneratorTempCoefficient.getAsDouble();
+    double heatValue =
+        temperature * TCAServerConfig.CONFIG.thermalGeneratorTempCoefficient.getAsDouble();
 
     if (isRaining) {
       heatValue *= TCAServerConfig.CONFIG.thermalGeneratorRainCoefficient.getAsDouble();
     }
 
     TCACuriosHelper.addEntityCoreEnergy(entity, Math.max((int) heatValue, 0));
+  }
+
+  @Override
+  public void appendHoverText(
+      ItemStack stack,
+      TooltipContext context,
+      List<Component> tooltipComponents,
+      TooltipFlag tooltipFlag) {
+    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+
+    tooltipComponents.add(
+        Component.translatable("thecyborgage.thermal_generator.tooltip")
+            .withStyle(ChatFormatting.GRAY));
   }
 }
