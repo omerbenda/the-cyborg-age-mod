04ed8f068ab780fbc1ac8ed7bd43a1332263afaa
omerbenda <omerbenda@gmail.com>
2026-05-29 18:10:16 +0300

    feat(Cyborg scout): Add cyborg beacon loot table and stop natural cyborg spawns

--- src/main/java/com/thecyborgage/events/EntityInitEventHandler.java
+++ src/main/java/com/thecyborgage/events/EntityInitEventHandler.java
@@ -1,15 +1,11 @@
 package com.thecyborgage.events;
 
 import com.thecyborgage.TheCyborgAgeMod;
-import com.thecyborgage.entities.CyborgEntity;
 import com.thecyborgage.init.TCAEntities;
-import net.minecraft.world.entity.SpawnPlacementTypes;
 import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.level.levelgen.Heightmap;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
-import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
 
 @EventBusSubscriber(modid = TheCyborgAgeMod.MOD_ID)
 public class EntityInitEventHandler {
@@ -17,15 +13,5 @@ public class EntityInitEventHandler {
   public static void onAttributeCreate(EntityAttributeCreationEvent event) {
     event.put(TCAEntities.CYBORG.get(), Monster.createMonsterAttributes().build());
     event.put(TCAEntities.CYBORG_SCOUT.get(), Monster.createMonsterAttributes().build());
-  }
-
-  @SubscribeEvent
-  public static void onRegisterSpawnPlacements(RegisterSpawnPlacementsEvent evt) {
-    evt.register(
-        TCAEntities.CYBORG.get(),
-        SpawnPlacementTypes.ON_GROUND,
-        Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-        CyborgEntity::checkMobSpawnRules,
-        RegisterSpawnPlacementsEvent.Operation.REPLACE);
   }
 }
